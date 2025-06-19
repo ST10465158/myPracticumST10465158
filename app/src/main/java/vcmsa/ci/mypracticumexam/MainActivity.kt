348812a094f120b09253160da5863665c291c062
ST10465158 <ST10465158@vcconnect.edu.za>
2025-06-19 11:38:35 +0200

    my first commit for my practicum

---
+++ app/src/main/java/vcmsa/ci/mypracticumexam/MainActivity.kt
@@ -0,0 +1,110 @@
+package vcmsa.ci.mypracticumexam
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.Rating
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    //Companion object to store parallel arrays
+    companion object {
+        val songTitle = mutableListOf<String>()
+        val artistName = mutableListOf<String>()
+        val rating = mutableListOf<Int>()
+        val comments = mutableListOf<String>()
+        //Author: Zahra Bulbula
+        //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //declarations for each category and button variables
+        val songnameInput = findViewById<EditText>(R.id.etSongTitle)
+        val artistnameInput = findViewById<EditText>(R.id.etArtistName)
+        val ratingsInput = findViewById<EditText>(R.id.etRatings)
+        val commentsInput = findViewById<EditText>(R.id.etComments)
+
+        val addButton = findViewById<Button>(R.id.btnAdd)
+        val goTosecond = findViewById<Button>(R.id.btnSecondScreen)
+        val exitButton = findViewById<Button>(R.id.btnExit)
+        //Author: Zahra Bulbula
+        //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+
+        addButton.setOnClickListener {
+            //declaration of variables for the button which will add these songs to playlist
+            val title = songnameInput.text.toString().trim()
+            val artist = artistnameInput.text.toString().trim()
+            val ratingStr = ratingsInput.text.toString().trim()
+            val comment = commentsInput.text.toString().trim()
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+
+            //Error handling to warn the user
+            if (title.isEmpty() || artist.isEmpty() || ratingStr.isEmpty()) {
+                Toast.makeText(this, "Please fill in all required fields.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+                //Author: Zahra Bulbula
+                //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+            }
+            //more error handling
+            val ratings = ratingStr.toIntOrNull()
+            if (ratings == null || ratings <= 0) {
+                Toast.makeText(this, "Enter a valid quantity (> 0).", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+                //Author: Zahra Bulbula
+                //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+            }
+
+            songTitle.add(title)
+            artistName.add(artist)
+            rating.add(ratingStr)
+            comments.add(comment)
+
+            Toast.makeText(this, "Song Added!", Toast.LENGTH_SHORT).show()
+
+            // Clear inputs
+            songnameInput.text.clear()
+            artistnameInput.text.clear()
+            ratingsInput.text.clear()
+            commentsInput.text.clear()
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+
+        }
+        //this button will take the user to the second screen
+        goTosecond.setOnClickListener {
+            startActivity(Intent(this, MainActivity2secondscreen::class.java))
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+        }
+        // this button will let the user exit the app
+        exitButton.setOnClickListener {
+            finish()
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+
+    }
+
+    }
+}
+//error fix
+private fun <E> MutableList<E>.add(element: String) {
+    //Author: Zahra Bulbula
+    //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+
+}
---
+++ app/src/main/java/vcmsa/ci/mypracticumexam/MainActivity2secondscreen.kt
@@ -0,0 +1,70 @@
+package vcmsa.ci.mypracticumexam
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2secondscreen : AppCompatActivity() {
+    private lateinit var outputText: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_activity2secondscreen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        outputText = findViewById(R.id.tvOutput)
+        //button declarations
+        val showsongsBtn = findViewById<Button>(R.id.btnShowSongs)
+        val showaverageBtn = findViewById<Button>(R.id.btnShowAverage)
+        val backBtn = findViewById<Button>(R.id.btnBack)
+        //Author: Zahra Bulbula
+        //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+
+        showsongsBtn.setOnClickListener {
+            //this button will diplay all the songs that the user entered when clicked
+            displayItems(showAll = true)
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+        }
+
+        showaverageBtn.setOnClickListener {
+            //this button will display the average rating of the songs the user entered
+            displayItems(showAll = false)
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+        }
+
+        backBtn.setOnClickListener {
+            //This button will take the user back to the main screen.
+            finish()
+            //Author: Zahra Bulbula
+            //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+        }
+    }
+    private fun displayItems(showAll: Boolean) {
+        val builder = StringBuilder()
+        //this will let the user see what is on their playlist list hen they click the "show list of songs" button
+        for (i in MainActivity.songTitle.indices) {
+            val qty = MainActivity.rating[i]
+            if (showAll || qty >= 0) {
+                builder.append("Song: ${MainActivity.songTitle[i]}\n")
+                builder.append("Artist: ${MainActivity.rating[i]}\n")
+                builder.append("Rating: $qty\n")
+                builder.append("Comments: ${MainActivity.comments[i]}\n\n")
+                //Author: Zahra Bulbula
+                //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+            }
+    }
+        //Error Handling
+        outputText.text = if (builder.isEmpty()) "No songs to show." else builder.toString()
+        //Author: Zahra Bulbula
+        //URL: https://github.com/zb662000/PracticumGuide/tree/master/app
+    }
+}
